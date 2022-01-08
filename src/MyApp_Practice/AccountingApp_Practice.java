2320051b924c74734ca61372cba04131ae352de3
Gyyu0 <96764471+Gyyu0@users.noreply.github.com>
2022-01-08 15:35:51 +0900

    2022-01-08

--- src/MyApp_Practice/AccountingApp_Practice.java
+++ src/MyApp_Practice/AccountingApp_Practice.java
@@ -4,14 +4,25 @@ public class AccountingApp_Practice {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Value of supply :" + 10000.0 );
-		System.out.println("VAT :" + (10000.0*0.1));
-		System.out.println("Total :" + (10000.0 + 10000.0*0.1));
-		System.out.println("Expense :" + (10000.0*0.3));
-		System.out.println("Income :" + (10000.0 - 10000.0*0.3));
-		System.out.println("Dividend 1 : " + (10000.0 - 10000.0*0.3) * 0.5);
-		System.out.println("Dividend 2 : " + (10000.0 - 10000.0*0.3) * 0.3);
-		System.out.println("Dividend 3 : " + (10000.0 - 10000.0*0.3) * 0.2);
+		double valueofsupply = Double.parseDouble(args[0]);
+		double vatRate = 0.1;
+		double expnseRate = 0.3;
+		double vat = valueofsupply * vatRate;
+		double total = valueofsupply * vat;
+		double expense = valueofsupply * expnseRate;
+		double income = valueofsupply - expense;
+		double dividen1 = income * 0.5;
+		double dividen2 = income * 0.3;
+		double dividen3 = income * 0.2;
+		
+		System.out.println("Value of supply :" + valueofsupply );
+		System.out.println("VAT :" + vat);
+		System.out.println("Total :" + total);
+		System.out.println("Expense :" + expense );
+		System.out.println("Income :" + income );
+		System.out.println("Dividend 1 : " + dividen1);
+		System.out.println("Dividend 2 : " + dividen2);
+		System.out.println("Dividend 3 : " + dividen3);
 
 	}
 
